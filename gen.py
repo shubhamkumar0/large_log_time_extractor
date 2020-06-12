ba5edef4f9073191b25a7fc21efc202997b05eda
shubhamkumar0 <shubham.kumar099@gmail.com>
2020-06-12 10:24:14 +0530

    added comment to gen file

--- gen.py
+++ gen.py
@@ -1,7 +1,8 @@
 import csv
 import re
 
-output_file = open('result2.txt','a')
+#script to write dummy dates in a file
+output_file = open('largefile.txt','a')
 for i in range(1000):
     output_file.write("{0}-06-08T01:49:48Z \n".format(1900+i))
 output_file.close()
